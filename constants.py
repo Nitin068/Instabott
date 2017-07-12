63229ec8fd4771e4172918eb7b1286d74c0f30cc
Nitin068 <e.r.nitinjassal123@gmail.com>
2017-07-12 13:13:55 +0530

    files added to github

---
+++ constants.py
@@ -0,0 +1,7 @@
+
+# global variables file
+
+# instagram API access token
+APP_ACCESS_TOKEN = '1673501107.115d3a6.14e80e47a7a94cd08c0c93d84a528cf7'
+# instagram API base url
+BASE_URL = 'https://api.instagram.com/v1/'
